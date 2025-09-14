1da3d77de3974bfbd9095bf05e309af733e4235e
skgamestournament <skgamestournament@gmail.com>
2025-09-14 18:57:07 +0530

    Create admin_dashboard_page.dart

---
+++ lib/admin_dashboard_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AdminDashboardPage extends StatelessWidget {
+  const AdminDashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Welcome to Admin Dashboard!'),
+      ),
+    );
+  }
+}
