3761757d99a5ec86c85889fbd5f390ebb4918b24
skgamestournament <skgamestournament@gmail.com>
2025-09-14 19:02:45 +0530

    Create admin_tournaments_page.dart

---
+++ lib/admin_tournaments_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdminTournamentsPage extends StatelessWidget {
+  const AdminTournamentsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Tournament Management Page - Coming Soon!'),
+    );
+  }
+}
